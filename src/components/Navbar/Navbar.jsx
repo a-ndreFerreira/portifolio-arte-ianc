58d58c9bbadbe98d729bb85358466ce2b66e4b0a
André Ferreira <tulibanet@hotmail.com>
2025-03-12 14:22:37 -0300

    navbar estruturada, inicializacao da home

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,6 +4,12 @@ import { NavLink, Link } from "react-router-dom"
 
 import { FaBarsStaggered } from "react-icons/fa6";
 
+import { FaBehance } from "react-icons/fa";
+
+import { FaInstagram } from "react-icons/fa";
+
+import { FaLinkedinIn } from "react-icons/fa";
+
 const Navbar = () => {
 
     const navbarContent = [
@@ -11,6 +17,12 @@ const Navbar = () => {
         { link: 'Projetos', path: '/projects' },
         { link: 'Sobre', path: '/about' },
         { link: 'Contato', path: '/contact' }
+    ]
+
+    const socialMedia = [
+        { link: <FaBehance />, to: 'http://behance.com' },
+        { link: <FaInstagram />, to: 'http://instagram.com' },
+        { link: <FaLinkedinIn />, to: 'http://linkedin.com' }
     ]
 
     return (
@@ -30,21 +42,40 @@ const Navbar = () => {
                 </button>
             </div>
 
-            <ul className={styles.ulNavbar}>
-                {
-                    navbarContent.map((item) => {
-                        const { link, path } = item;
+            <div className={styles.divListsNavbar}>
+                <ul className={styles.ulNavbar}>
+                    {
+                        navbarContent.map((item) => {
+                            const { link, path } = item;
 
-                        return (
-                            <li key={path}>
-                                <NavLink to={path} className={({ isActive }) => (isActive ? styles.active : '')}>
-                                    {link}
-                                </NavLink>
-                            </li>
-                        )
-                    })
-                }
-            </ul>
+                            return (
+                                <li key={path}>
+                                    <NavLink to={path} className={({ isActive }) => (isActive ? styles.active : '')}>
+                                        {link}
+                                    </NavLink>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+
+                <ul className={styles.ulSocialMedia}>
+                    {
+                        socialMedia.map((item) => {
+                            const { link, to } = item;
+
+                            return (
+                                <li key={to}>
+                                    <Link to={to} target='_blank'>
+                                        {link}
+                                    </Link>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
+
         </nav>
     )
 }
--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,32 +1,51 @@
 import styles from './Home.module.css'
 
+import imgBg from '../../assets/double.jpeg'
+
+import imgEye from '../../assets/eyeIanc.png'
+
 const Home = () => {
     return (
-        <section>
-            <div>
-                background here
-            </div>
-            <div>
-                sleeve here
-            </div>
-            <div className={styles.container}>
-
-                <div className={styles.hero}>
-                    <img src="" alt="Deus Olho - Arte Ianc - Bianca Santos" />
-                </div>
-
-                <div className={styles.containerContent}>
-                    <div className={styles.arteContent}>
-                        Arte
+        <div className={styles.container}>
+            <section>
+                <div className={styles.containerSection}>
+                    <div className={styles.divImgBg}>
+                        <img src={imgBg} alt="RandomBg" />
                     </div>
 
-                    <div className={styles.iancContent}>
-                        IA<span>N</span>C
+                    <div className={styles.sleeve}></div>
+
+                    <div className={styles.containerHero}>
+
+                        <div className={styles.hero}>
+                            <img src={imgEye} alt="Deus Olho - Arte Ianc - Bianca Santos" />
+                        </div>
+
+                        <div className={styles.containerContent}>
+                            <div className={styles.arteContent}>
+                                Arte
+                            </div>
+
+                            <div className={styles.iancContent}>
+                                IA<span>N</span>C
+                            </div>
+                        </div>
+
                     </div>
+
                 </div>
 
-            </div>
-        </section>
+            </section>
+
+            <section>
+                <div>
+                    <h2>
+                        Aqui a proxima section
+                    </h2>
+                </div>
+            </section>
+
+        </div>
     )
 }
 
